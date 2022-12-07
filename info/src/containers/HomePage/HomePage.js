17b20199105ecb910d09c4d95f12782dd21be603
zyhzyh0333 <yihuazho@gmail.com>
2022-12-07 16:28:53 -0500

    modified twitter mainPage, fixed some bugs

--- info/src/containers/HomePage/HomePage.js
+++ info/src/containers/HomePage/HomePage.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import ChatPage from "../ChatPage/ChatPage";
 import chaticon from "../HomePage/chat.png";
 import axios from "axios";
+import finalTrends from "../TwitterPage/components/finalTrends";
 
 function HomePage() {
   let navigate = useNavigate();
@@ -48,6 +49,15 @@ function HomePage() {
     } else {
       return day.toLocaleString("en-us", { weekday: "long" });
     }
+  }
+
+  function getUSTrendName(index) {
+    const USTrendsSet = finalTrends["23424977"];
+    return USTrendsSet[index].name;
+  }
+  function getUSTrendLink(index) {
+    const USTrendsSet = finalTrends["23424977"];
+    return USTrendsSet[index].url;
   }
 
   const [city, setCity] = useState("");
@@ -165,6 +175,113 @@ function HomePage() {
             <h2 className="card-title">Twitter</h2>
             <p>twitter</p>
             <img src="images/icon-calculator.svg" alt="" />
+
+            <h1
+              style={{
+                color: "#1da1f2",
+                textAlign: "center",
+                marginTop: "-10%",
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="35"
+                height="35"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#1da1f2"
+                  d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                />
+              </svg>
+              <span> </span>
+              Twitter trends
+            </h1>
+            <h4
+              style={{
+                textAlign: "center",
+                marginBottom: "20px",
+              }}
+            >
+              Current trends in the U.S.
+            </h4>
+            <div
+              style={{
+                textAlign: "center",
+                border: "3px solid #1da1f2",
+                borderRadius: "20px",
+                width: "50%",
+                marginLeft: "25%",
+                marginBottom: "10px",
+                height: "40px",
+                fontSize: "20px",
+                fontWeight: "600",
+              }}
+            >
+              <a
+                style={{
+                  textDecoration: "none",
+                  color: "#1da1f2",
+                }}
+                href={getUSTrendLink(0)}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {getUSTrendName(0)}
+              </a>
+            </div>
+            <div
+              style={{
+                textAlign: "center",
+                border: "3px solid #1da1f2",
+                borderRadius: "20px",
+                width: "50%",
+                marginLeft: "25%",
+                marginBottom: "10px",
+
+                height: "40px",
+                fontSize: "20px",
+                fontWeight: "600",
+              }}
+            >
+              <a
+                style={{
+                  textDecoration: "none",
+                  color: "#1da1f2",
+                }}
+                href={getUSTrendLink(1)}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {getUSTrendName(1)}
+              </a>
+            </div>
+            <div
+              style={{
+                textAlign: "center",
+                border: "3px solid #1da1f2",
+                borderRadius: "20px",
+                width: "50%",
+                marginLeft: "25%",
+                marginBottom: "10px",
+
+                height: "40px",
+                fontSize: "20px",
+                fontWeight: "600",
+              }}
+            >
+              <a
+                style={{
+                  textDecoration: "none",
+                  color: "#1da1f2",
+                }}
+                href={getUSTrendLink(2)}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {getUSTrendName(2)}
+              </a>
+            </div>
           </div>
         </div>
         <div onClick={chatonclick}>
